9ba28d474705d7f3f9077e6a8b946e9d59c40beb
Jacob Von Ogden <jvon1197@gmail.com>
2021-12-24 10:10:44 -0500

    added Routing, setup initial navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App"> Hello from App.JS </div>
+    <div className="App">
+      Hello From App
+      <Router>
+        <NavBar />
+      </Router>
+    </div>
   );
 };
 
